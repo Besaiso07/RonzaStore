bbb75d2586c69b3fa1ac6755c6a7d14cb41bfea9
Besaiso07 <mahmoud.besaiso@gmail.com>
2023-03-07 15:51:58 +0200

    frist commit

---
+++ ronzastore/views.py
@@ -0,0 +1,15 @@
+from django.template import loader
+from django.http import HttpResponseServerError
+from django.shortcuts import render 
+from store.models import Product
+
+def home(request):
+        products = Product.objects.all().filter(is_available=True)
+        
+        context = {
+                'products' : products,
+        }
+
+
+        return render(request, 'home.html',context)
+
---
+++ store/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('store/', views.store_html, name='store_html'),
+    path('store/<slug:category_slug>/', views.store_html, name='product_by_category'),
+    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+]
