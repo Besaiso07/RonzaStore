51e4cf0210068e5e3dbb3098c5af2d700fc5ebc2
Besaiso07 <mahmoud.besaiso@gmail.com>
2023-03-18 11:32:35 +0200

    store & cart

--- ronzastore/settings.py
+++ ronzastore/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'carts',
     'accounts',
     'category',
     'store',
@@ -66,7 +67,9 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 ## new 
-                'category.contex_procesors.menu_links'
+                'category.contex_procesors.menu_links',
+                'carts.context_processore.counter',
+                
             ],
         },
     },
--- store/urls.py
+++ store/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('store/', views.store_html, name='store_html'),
-    path('store/<slug:category_slug>/', views.store_html, name='product_by_category'),
-    path('store/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('store/category/<slug:category_slug>/', views.store_html, name='product_by_category'),
+    path('store/category/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
+    path('store/search/', views.search, name='search'),
 ]
